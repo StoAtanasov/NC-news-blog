e11a814ba2220b78dad548288b4d5de3fc6abf0f
StoAtanasov <stoatanasov@yahoo.com>
2019-09-16 10:25:11 +0100

    ArticlesList list component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from './components/header';
 import Home from './components/home';
+import {Router} from '@reach/router'
+import ArticlesList from './components/artcilesList';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+    <Router>
+      <Home path ="/"/>
+      <ArticlesList path="/articles"/>
+    </Router>
       Hello World
     </div>
   )
---
+++ src/components/artcilesList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ArticlesList extends Component {
+ state = {
+
+ }
+  render() { 
+    return ( <p>Artciles list</p> );
+  }
+}
+ 
+export default ArticlesList;
